ca01c0179bb41c7619018914f70d869ca4496244
randomusernamea <stars.a@outlook.com>
2023-02-13 20:57:42 -0300

    First commit

---
+++ colaboradores/src/components/Skynet.jsx
@@ -0,0 +1,16 @@
+import {react} from 'react'
+
+function Skynet(props){
+    return (
+        <>
+            <h1>Hello humans, welcome to the future!</h1>
+            <p>Skynet is the future, coming Soon™</p>
+            
+        </>
+    )
+}
+
+
+
+
+export default Skynet;
