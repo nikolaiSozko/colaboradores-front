f50ff5d3a2cb9b2b5dfbec116453622c646efe5d
sabirnamato <sabir.mato00@gmail.com>
2023-02-13 21:08:14 -0300

    un componente mas - sabri

---
+++ colaboradores/src/components/Sabrina.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sabrina() {
+  return (
+    <>
+      <h1>Componente Lorem</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis rerum
+        maxime quam, alias voluptas similique minima illo eius aut corporis,
+        suscipit accusantium possimus velit at, hic nulla pariatur assumenda
+        consequatur.
+      </p>
+    </>
+  );
+}
+
+export default Sabrina;
